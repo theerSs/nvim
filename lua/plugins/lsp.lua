bd95ba90e519077d2d578747bebcd0d461e8109c
erS <jakublasek41@gmail.com>
2025-04-19 22:33:13 +0200

    Remove csharp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -34,7 +34,6 @@ require('mason-lspconfig').setup({
       'docker_compose_language_service',
       'dockerls', 
       'graphql', 
-      'csharp_ls',
       'marksman',
       'html',
       'jsonls',
